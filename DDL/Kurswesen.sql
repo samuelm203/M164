ef27870eb5b542dd7b1ec49e099bc1085b74fb37
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2026-03-16 14:51:58 +0100

    Initial Commit

---
+++ DDL/Kurswesen.sql
@@ -0,0 +1,17 @@
+-- Sicherstellen das man auf Master ist
+USE master;
+
+-- Datenbank "Kurswesen" erstellen und auf die erstellte Datenbank wechseln 
+CREATE DATABASE Kurswesen;
+
+USE Kurswesen; 
+
+-- Tabelle "Kurs" erstellen
+CREATE TABLE Kurs (
+	KursID INT IDENTITY(1,1) PRIMARY KEY, 
+	BEZ NVARCHAR NOT NULL, 
+	MaxTeilnehmer INT NOT NULL, 
+	Kosten INT NOT NULL, 
+	StartDatum DATE NOT NULL, 
+	Dauer DECIMAL(2,1) NOT NULL
+);
